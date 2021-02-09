4a00e12c51ca320320e35469256f384e82bf24b6
Hleb Albau <hleb.albau@gmail.com>
2021-02-09 13:51:27 +0300

    Fix generator version

--- update-models.sh
+++ update-models.sh
@@ -3,12 +3,12 @@
 ENUM_MAPPINGS="ImageSource=Integer,FaceRecognitionResultCode=Integer,LivenessStatus=Integer" \
 \
 && docker run --rm -v "${PWD}:/client" -v "${PWD}/../FaceRecognition-web-openapi:/definitions" \
-openapitools/openapi-generator-cli generate \
+openapitools/openapi-generator-cli:v5.0.0-beta2 generate \
 -i /definitions/index.yml -g java -o /client/client \
 -c /client/java-generator-config.json -t /client/client/generator-templates/ \
 \
 && docker run --rm -v "${PWD}:/client" -v "${PWD}/../FaceRecognition-web-openapi:/definitions" \
-openapitools/openapi-generator-cli generate \
+openapitools/openapi-generator-cli:v5.0.0-beta2 generate \
 -i /definitions/index.yml -g java -o /client/client \
 -c /client/java-generator-config.json -t /client/client/generator-templates/ \
 --import-mappings $ENUM_MAPPINGS || exit 1
