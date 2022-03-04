ed09c0cd1e0637cdcefba93cb17029c1d22259c7
Danila Kislitsyn <dangost16@gmail.com>
2022-03-04 18:19:07 +0300

    add tag into example

--- example/src/main/java/com/regula/facesdk/webclient/Main.java
+++ example/src/main/java/com/regula/facesdk/webclient/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         var apiBasePath = System.getenv(API_BASE_PATH);
         if (apiBasePath == null) {
-            apiBasePath = "http://localhost:41101";
+            apiBasePath = "https://faceapi.regulaforensics.com";
         }
         byte[] face1 = readFile("face_1.jpg");
         byte[] face2 = readFile("face_2.jpg");
@@ -22,9 +22,13 @@ public class Main {
         var matchImage1 = new MatchImage().index(0).data(face1).type(ImageSource.LIVE);
         var matchImage2 = new MatchImage().index(1).data(face1).type(ImageSource.DOCUMENT_RFID);
         var matchImage3 = new MatchImage().index(3).data(face2).type(ImageSource.LIVE);
-        var matchRequest = new MatchRequest().images(
-                List.of(matchImage1, matchImage2, matchImage3)
-        );
+
+        var matchImages = List.of(matchImage1, matchImage2, matchImage3);
+
+        var matchRequest = new MatchRequest()
+                .images(matchImages)
+                .tag("1");
+
         var matchResponse = sdk.matchingApi.match(matchRequest);
 
         System.out.println("-----------------------------------------------------------------");
@@ -37,7 +41,7 @@ public class Main {
             );
         }
 
-        var detectRequest = new DetectRequest().image(face2);
+        var detectRequest = new DetectRequest().image(face2).tag("1");
         var detectResponse = sdk.matchingApi.detect(detectRequest);
         var detectResults = detectResponse.getResults();
 
