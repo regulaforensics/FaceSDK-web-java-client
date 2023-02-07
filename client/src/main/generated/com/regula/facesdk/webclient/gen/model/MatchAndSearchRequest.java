d21cf3273f4fb31558001d63d86b0a19acf2bd66
GitHub Action <action@github.com>
2023-02-07 13:13:17 +0000

    update-clients

--- client/src/main/generated/com/regula/facesdk/webclient/gen/model/MatchAndSearchRequest.java
+++ client/src/main/generated/com/regula/facesdk/webclient/gen/model/MatchAndSearchRequest.java
@@ -114,11 +114,11 @@ public class MatchAndSearchRequest {
   }
 
    /**
-   * The similarity distance threshold.
+   * The similarity distance threshold, should be between 0.0 and 2.0, where 0.0 is for returning results for only the most similar persons and 2.0 is for all the persons, even the dissimilar ones. If not set, the default 1.0 value is used.
    * @return threshold
   **/
   @javax.annotation.Nullable
-  @ApiModelProperty(value = "The similarity distance threshold.")
+  @ApiModelProperty(value = "The similarity distance threshold, should be between 0.0 and 2.0, where 0.0 is for returning results for only the most similar persons and 2.0 is for all the persons, even the dissimilar ones. If not set, the default 1.0 value is used.")
 
   public Float getThreshold() {
     return threshold;
--- client/src/main/generated/com/regula/facesdk/webclient/gen/model/SearchParameters.java
+++ client/src/main/generated/com/regula/facesdk/webclient/gen/model/SearchParameters.java
@@ -77,11 +77,11 @@ public class SearchParameters {
   }
 
    /**
-   * The similarity distance threshold.
+   * The similarity distance threshold, should be between 0.0 and 2.0, where 0.0 is for returning results for only the most similar persons and 2.0 is for all the persons, even the dissimilar ones. If not set, the default 1.0 value is used.
    * @return threshold
   **/
   @javax.annotation.Nullable
-  @ApiModelProperty(value = "The similarity distance threshold.")
+  @ApiModelProperty(value = "The similarity distance threshold, should be between 0.0 and 2.0, where 0.0 is for returning results for only the most similar persons and 2.0 is for all the persons, even the dissimilar ones. If not set, the default 1.0 value is used.")
 
   public Float getThreshold() {
     return threshold;
